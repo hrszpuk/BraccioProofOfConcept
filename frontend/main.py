3f2f229c98a818dcca027c979ac4b98a66d031e6
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-06-25 15:22:11 +0100

    Finished frontend (for now)

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,27 @@
 import time
 import serial
 
+help = """
+Instructions:
+    SETUP                   - All motors enter default position
+    ROTATE [ID] [DEGREES]   - Rotate a motor a number of degrees
+
+Motors:
+    M1 (base)
+    M2 (shoulder)
+    M3 (elbow)
+    M4 (wrist vertical)
+    M5 (wrist rotation)
+    M6 (gripper)
+
+NOTE: Degrees must be between 0 and 180 (motor limits)
+
+Examples:
+    >> SETUP
+    >> ROTATE M1 180 
+    >> ROTATE M3 90
+"""
+
 print("ARDUINO BRACCIO ROBOT ARM CONTROLLER - Proof Of Concept")
 
 port = input("Enter the port of your Arduino: ")
@@ -15,10 +36,32 @@ time.sleep(1)
 print(">> type \"help\" for a list of instructions")
 
 while True:
-    commands = input(">> ")
+    command = input(">> ").strip(" \n\r\t").upper()
+    if command == "HELP":
+        print(help)
+    elif command == "SETUP":
+        send = arduino.write(bytes("S0000", "utf-8"))
+    elif command == "QUIT":
+        print("Closing serial connection with the Arduino")
+        arduino.close()
+    else:
+        command = command.split(" ")
+        if command[0] != "ROTATE":
+            print("Unknown command")
+            continue
+        elif len(command) != 3:
+            print("ROTATE command takes 2 parameters: [ID] [DEGREES]")
+        else:
+            instruction = "R"
+            motor = command[1].removeprefix("M")
+            degrees = command[2]
 
-send = arduino.write(bytes("S0000", "utf-8"))
-print(f"Sent {send} bytes")
-time.sleep(0.1)
-print(f"Read: {arduino.read_all()}")
-arduino.close()
+            if len(degrees) < 2:
+                degrees = "00" + degrees
+            elif len(degrees) < 3:
+                degrees = "0" + degrees
+
+            instruction += motor + degrees
+
+            send = arduino.write(bytes(instruction, "utf-8"))
+
